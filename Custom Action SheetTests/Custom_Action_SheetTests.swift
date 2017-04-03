f17cc7fde85b805fa557389ded6cdbd065cd5564
rhnsharma999 <rhnsharma999@gmail.com>
2017-04-03 21:46:53 +0530

    Initial Commit

---
+++ Custom Action SheetTests/Custom_Action_SheetTests.swift
@@ -0,0 +1,36 @@
+//
+//  Custom_Action_SheetTests.swift
+//  Custom Action SheetTests
+//
+//  Created by Rohan Lokesh Sharma on 03/04/17.
+//  Copyright © 2017 webarch. All rights reserved.
+//
+
+import XCTest
+@testable import Custom_Action_Sheet
+
+class Custom_Action_SheetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
